34bc62825668bc592fdd3de12bd65b1070f398c2
Damla Yıldırım <damla.yildirim@mono.net.tr>
2020-06-28 23:26:26 +0300

    pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,28 @@ import { AppComponent } from './app.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { UserPanelComponent } from './user-panel/user-panel.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { BurgerNavComponent } from './home/burger-nav/burger-nav.component';
+import { ServicesSectionComponent } from './home/services-section/services-section.component';
+import { TestimonialsSectionComponent } from './home/testimonials-section/testimonials-section.component';
+import { ProductsSectionComponent } from './home/products-section/products-section.component';
+import { ClientSectionComponent } from './home/client-section/client-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminPanelComponent,
     UserPanelComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    BurgerNavComponent,
+    ServicesSectionComponent,
+    TestimonialsSectionComponent,
+    ProductsSectionComponent,
+    ClientSectionComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
